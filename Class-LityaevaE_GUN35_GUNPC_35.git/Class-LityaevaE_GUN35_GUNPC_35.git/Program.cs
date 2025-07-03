9209e440f61c0b83157830cb96992a9f91944466
Lityaeva Elvira <lityaevae.e@gmail.com>
2025-07-03 22:30:18 +0700

    Armor

--- Class-LityaevaE_GUN35_GUNPC_35.git/Class-LityaevaE_GUN35_GUNPC_35.git/Program.cs
+++ Class-LityaevaE_GUN35_GUNPC_35.git/Class-LityaevaE_GUN35_GUNPC_35.git/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+using Class_LityaevaE_GUN35_GUNPC_35.git;
+
+Console.WriteLine("Подготовка к бою:");
+string name = Console.ReadLine();
+Console.WriteLine("Введите начальное здоровье бойца (10-100):");
+int.TryParse(s: Console.ReadLine(), out int health);
+var unit = new Unit (name, health);
